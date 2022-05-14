8febff8f7a57e886a994f1da6ed8ea6a0e733fb9
saryunaBadmaeva <90922702+saryunaBadmaeva@users.noreply.github.com>
2022-05-14 21:52:05 +0700

    lab11

---
+++ LAb11/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace LAb11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelChiSquared = new System.Windows.Forms.Label();
+            this.labelVariance = new System.Windows.Forms.Label();
+            this.labelAverage = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btStart = new System.Windows.Forms.Button();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            this.numVariance = new System.Windows.Forms.NumericUpDown();
+            this.numMean = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVariance)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMean)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelChiSquared);
+            this.panel1.Controls.Add(this.labelVariance);
+            this.panel1.Controls.Add(this.labelAverage);
+            this.panel1.Location = new System.Drawing.Point(3, 512);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1192, 165);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelChiSquared
+            // 
+            this.labelChiSquared.AutoSize = true;
+            this.labelChiSquared.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelChiSquared.Location = new System.Drawing.Point(274, 118);
+            this.labelChiSquared.Name = "labelChiSquared";
+            this.labelChiSquared.Size = new System.Drawing.Size(0, 46);
+            this.labelChiSquared.TabIndex = 7;
+            // 
+            // labelVariance
+            // 
+            this.labelVariance.AutoSize = true;
+            this.labelVariance.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelVariance.Location = new System.Drawing.Point(274, 66);
+            this.labelVariance.Name = "labelVariance";
+            this.labelVariance.Size = new System.Drawing.Size(0, 46);
+            this.labelVariance.TabIndex = 6;
+            // 
+            // labelAverage
+            // 
+            this.labelAverage.AutoSize = true;
+            this.labelAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAverage.Location = new System.Drawing.Point(274, 15);
+            this.labelAverage.Name = "labelAverage";
+            this.labelAverage.Size = new System.Drawing.Size(0, 46);
+            this.labelAverage.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btStart);
+            this.panel2.Controls.Add(this.numSize);
+            this.panel2.Controls.Add(this.numVariance);
+            this.panel2.Controls.Add(this.numMean);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(274, 506);
+            this.panel2.TabIndex = 1;
+            // 
+            // btStart
+            // 
+            this.btStart.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btStart.Location = new System.Drawing.Point(15, 198);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(245, 78);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = false;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // numSize
+            // 
+            this.numSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numSize.Location = new System.Drawing.Point(171, 117);
+            this.numSize.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(100, 39);
+            this.numSize.TabIndex = 7;
+            this.numSize.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numVariance
+            // 
+            this.numVariance.DecimalPlaces = 2;
+            this.numVariance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numVariance.Location = new System.Drawing.Point(140, 65);
+            this.numVariance.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numVariance.Name = "numVariance";
+            this.numVariance.Size = new System.Drawing.Size(106, 39);
+            this.numVariance.TabIndex = 6;
+            this.numVariance.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numMean
+            // 
+            this.numMean.DecimalPlaces = 2;
+            this.numMean.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numMean.Location = new System.Drawing.Point(126, 12);
+            this.numMean.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numMean.Name = "numMean";
+            this.numMean.Size = new System.Drawing.Size(105, 39);
+            this.numMean.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(9, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sample size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Variance";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mean";
+            // 
+            // chart1
+            // 
+            chartArea5.AxisY.Maximum = 0.7D;
+            chartArea5.AxisY.Minimum = 0D;
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Location = new System.Drawing.Point(283, 0);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Name = "Series1";
+            series5.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(912, 506);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 679);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVariance)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMean)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelChiSquared;
+        private System.Windows.Forms.Label labelVariance;
+        private System.Windows.Forms.Label labelAverage;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown numSize;
+        private System.Windows.Forms.NumericUpDown numVariance;
+        private System.Windows.Forms.NumericUpDown numMean;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ LAb11/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAb11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double mean, variance;
+        int size;
+
+        double empiricMean, empiricVariance;
+        double errE, errD;
+        double chiSquared;
+        double[] chiSquaredValues = new double[15] {3.841, 5.991, 7.815, 9.488, 11.070, 12.592, 14.067, 15.507, 
+                                                    16.919, 18.307, 19.675, 21.026, 22.362, 23.685, 24.996};
+
+        int k;
+        double spanLength;
+        int[] spanCount;
+        double[] spanFrequency;
+        double[] spanLen;
+        double max = -99999, min = 99999;
+        double[] normalDis = new double[10000];
+
+        double curLength;
+
+        Random rnd = new Random();
+        double l1, l2;
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            max = 0; min = 0; chiSquared = 0;
+            mean = (double)numMean.Value;
+            variance = (double)numVariance.Value;
+            size = (int)numSize.Value;
+            
+            for (int i = 0; i < size; i+=2)
+            {
+                l1 = rnd.NextDouble();
+                l2 = rnd.NextDouble();
+                normalDis[i] = Math.Sqrt((-2) * Math.Log(l1)) * Math.Sin(2 * Math.PI * l2) * Math.Sqrt(variance) + mean;
+                normalDis[i + 1] = Math.Sqrt((-2) * Math.Log(l1)) * Math.Cos(2 * Math.PI * l2) * Math.Sqrt(variance) + mean;
+                if (normalDis[i] > max)
+                    max = normalDis[i];
+                if (normalDis[i] < min)
+                    min = normalDis[i];
+                if (normalDis[i + 1] > max)
+                    max = normalDis[i + 1];
+                if (normalDis[i + 1] < min)
+                    min = normalDis[i + 1];
+
+                empiricMean += normalDis[i] + normalDis[i + 1];
+                empiricVariance += normalDis[i] * normalDis[i] + normalDis[i + 1] * normalDis[i + 1];
+            }
+            
+            empiricMean = empiricMean / size;
+            empiricVariance = empiricVariance / size - empiricMean * empiricMean;
+            errE = Math.Abs(empiricMean - mean) / Math.Abs(mean) * 100;
+            errD = Math.Abs(empiricVariance - variance) / Math.Abs(variance) * 100;
+            labelAverage.Text = "Average: " + Math.Round(empiricMean, 2).ToString() + 
+                                "(error = " + Math.Round(errE, 2) + "%)";
+            labelVariance.Text = "Variance: " + Math.Round(empiricVariance, 2).ToString() + 
+                                "(error = " + Math.Round(errD, 2) + "%)";
+
+            k = (int)Math.Log(size, 2) + 1;
+            spanLength = (max - min) / k;
+            spanCount = new int[k];
+            for(int i = 0; i < size; i++)
+            {
+                curLength = min + spanLength;
+                int j = 0;
+                while (j < k)
+                {
+                    if (normalDis[i] < curLength)
+                    {
+                        spanCount[j]++;
+                        break;
+                    }
+                    else
+                    {
+                        curLength += spanLength;
+                        j++;
+                    }   
+                }
+                normalDis[i] = 0;
+            }
+            
+            spanFrequency = new double[k];
+            spanLen = new double[k + 1];
+            double min1 = min;
+            double pi = 0, px = 0;
+            double x = 0;
+            for (int i = 0; i < k; i++)
+            {
+                spanFrequency[i] = (double)spanCount[i] / size;
+                x = (min1 + min1 + spanLength) / 2;
+                px = (1 / (Math.Sqrt(2 * Math.PI) * Math.Sqrt(variance))) *
+                    Math.Exp(-((x - mean) * (x - mean)) / (2 * variance));
+                pi = ((min1 + spanLength) - min1) * px;
+                chiSquared += (spanCount[i] * spanCount[i]) / (double)(size * pi); 
+                spanLen[i] = min1;
+                chart1.Series[0].Points.AddXY(min1, spanFrequency[i]);
+                min1 += spanLength;
+            }
+            chart1.Series[0].Points.AddXY(min1, spanFrequency[k - 1]);
+            //chart1.ChartAreas[0].AxisX.Minimum = min - 0.01;
+            //chart1.ChartAreas[0].AxisX.Maximum = max + 0.01;
+            chart1.ChartAreas[0].AxisX.Minimum = -5;
+            chart1.ChartAreas[0].AxisX.Maximum = 5;
+
+            chiSquared -= size;
+            labelChiSquared.Text = "Chi-squared: " + Math.Round(chiSquared, 2) + " > " + chiSquaredValues[k - 1].ToString() + " is ";
+            if (chiSquared > chiSquaredValues[k - 2])
+            {
+                labelChiSquared.Text += "True";
+            }
+            else
+                labelChiSquared.Text += "False";
+        }
+    }
+}
